adbe47e74fcaf44fa082dac5dfed2bdd12cfafbd
Lachlan Jordan <lachlan.jordan@protonmail.com>
2022-06-24 08:33:40 +1000

    Updated endpoints

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGrpc()
        .AddJsonTranscoding();
 
-//builder.Services.AddGrpcClient<Racing.Racing.RacingClient>(o => o.Address = new Uri("http://localhost:7000"));
+builder.Services.AddGrpcClient<Racing.Racing.RacingClient>(o => o.Address = new Uri("http://localhost:7000"));
 builder.Services.AddGrpcClient<Sports.Sports.SportsClient>(o => o.Address = new Uri("http://localhost:7100"));
 
 var app = builder.Build();
--- API/Services/SportsService.cs
+++ API/Services/SportsService.cs
@@ -12,14 +12,14 @@ public class SportsService : Sports.Sports.SportsBase
         _client = client;
     }
 
-    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:6000)
+    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:7100)
     public override async Task<ListEventsResponse> ListEvents(ListEventsRequest request, ServerCallContext context)
     {
         return await _client.ListEventsAsync(request,
                                              Metadata.Empty);
     }
 
-    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:6000)
+    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:7100)
     public override async Task<Event> GetEvent(GetEventRequest request, ServerCallContext context)
     {
         return await _client.GetEventAsync(request, Metadata.Empty);
