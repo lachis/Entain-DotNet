0665242994974bf683eadff3a82c00c7cfbe7690
Lachlan Jordan <lachlan.jordan@protonmail.com>
2022-06-23 19:55:08 +1000

    tests

--- Racing/Tests/Racing.UnitTests/UnitTests/RacingServiceTests.cs
+++ Racing/Tests/Racing.UnitTests/UnitTests/RacingServiceTests.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using Google.Protobuf.WellKnownTypes;
 using Infrastructure.Contracts;
 using Moq;
@@ -204,5 +205,91 @@ public class RacingServiceTests
         Assert.Equal(2,
                      response.Races[1]
                              .Id);
+    }
+
+     [Fact]
+    public async Task ListRaces_Filter_With_Visible_OrderBy_Start_Time_Returns_Visible_Only_OrderedBy_Start_Time()
+    {
+        // Arrange
+        var mockRepo = new Mock<IRaceRepository>();
+        mockRepo.Setup(m => m.List(It.IsAny<ListRacesRequestFilter>(),It.IsAny<ListRacesRequestOrder>()))
+                .Returns(new List<Race>
+                         {
+                             new Race
+                             {
+                                 AdvertisedStartTime = Timestamp.FromDateTime(DateTime.SpecifyKind(new DateTime(2021,
+                                                                                                                01,
+                                                                                                                01),
+                                                                                                   DateTimeKind.Utc)),
+                                 Id = 1,
+                                 MeetingId = 2,
+                                 Name = "Test",
+                                 Number = 1,
+                                 Visible = true
+                             },
+                             new Race
+                             {
+                                 Id = 2,
+                                 MeetingId = 2,
+                                 Name = "Test2",
+                                 Number = 2,
+                                 Visible = true,
+                                 AdvertisedStartTime = Timestamp.FromDateTime(DateTime.SpecifyKind(new DateTime(2019,
+                                                                                                                01,
+                                                                                                                01),
+                                                                                                   DateTimeKind.Utc))
+                             },
+                             new Race
+                             {
+                                 AdvertisedStartTime = Timestamp.FromDateTime(DateTime.SpecifyKind(new DateTime(2017,
+                                                                                                                01,
+                                                                                                                01),
+                                                                                                   DateTimeKind.Utc)),
+                                 Id = 4,
+                                 MeetingId = 2,
+                                 Name = "Test4",
+                                 Number = 1,
+                                 Visible = true
+                             },
+                             new Race
+                             {
+                                 Id = 5,
+                                 MeetingId = 2,
+                                 Name = "Test5",
+                                 Number = 2,
+                                 Visible = true,
+                                 AdvertisedStartTime = Timestamp.FromDateTime(DateTime.SpecifyKind(new DateTime(2016,
+                                                                                                                01,
+                                                                                                                01),
+                                                                                                   DateTimeKind.Utc))
+                             }
+                         }.OrderBy(x=>x.AdvertisedStartTime).ToList().AsReadOnly);
+        var service = new RacingService(mockRepo.Object);
+
+        // Act
+        var response = await service.ListRaces(new ListRacesRequest
+                                               {
+                                                   Filter = new ListRacesRequestFilter
+                                                            {
+                                                                OnlyVisibleRaces = true
+                                                            },
+                                                   Order = new ListRacesRequestOrder()
+                                                           {
+                                                               Field = "advertised_start_time"
+                                                           }
+                                               },
+                                               TestServerCallContext.Create());
+
+        // Assert
+        var responseRaces = response.Races;
+        responseRaces.Should()
+                     .BeInAscendingOrder(x => x.AdvertisedStartTime);
+
+        responseRaces.Should()
+                     .HaveCount(4);
+
+        responseRaces.Should()
+                     .OnlyContain(x => x.Visible);
+
     }
 }
