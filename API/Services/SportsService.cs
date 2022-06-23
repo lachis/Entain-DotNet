b8bc0fe1aa1518fbc9abce912257942e7fcab762
Lachlan Jordan <lachlan.jordan@protonmail.com>
2022-06-24 08:31:30 +1000

    Finalising Task 5

---
+++ API/Services/SportsService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Sports;
+
+namespace API.Services;
+
+public class SportsService : Sports.Sports.SportsBase
+{
+    private readonly Sports.Sports.SportsClient _client;
+
+    public SportsService(Sports.Sports.SportsClient client)
+    {
+        _client = client;
+    }
+
+    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:6000)
+    public override async Task<ListEventsResponse> ListEvents(ListEventsRequest request, ServerCallContext context)
+    {
+        return await _client.ListEventsAsync(request,
+                                             Metadata.Empty);
+    }
+
+    // HTTP Service wraps gRPC client which will forward the request to the gRPC Racing service (ie at http://localhost:6000)
+    public override async Task<Event> GetEvent(GetEventRequest request, ServerCallContext context)
+    {
+        return await _client.GetEventAsync(request, Metadata.Empty);
+    }
+}
